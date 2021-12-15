143aa0fa364eff655f46460b6d3d7c5a2457f43e
BenjaminGiez <benjamin.giez@outlook.fr>
2021-12-15 09:17:20 +0100

    j'aprecie les fruits au sirop

---
+++ add_question.php
@@ -0,0 +1,75 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>Éditer Question</title>
+</head>
+<body>
+
+<form action="add_question.php" method="post">
+            <div class="c100">
+                <label for="quest">Question : </label>
+                <input type="text" id="quest" name="quest">
+            </div>
+            <div class="c100">
+                <label for="rep">Réponse A : </label>
+                <input type="repA" id="repA" name="repA">
+            </div>
+            <div class="c100">
+                <label for="rep">Réponse B : </label>
+                <input type="repB" id="repB" name="repB">
+            </div>
+            <div class="c100">
+                <label for="rep">Réponse C : </label>
+                <input type="repC" id="repC" name="repC">
+            </div>
+            <div class="c100">
+                <label for="rep">Réponse D : </label>
+                <input type="repD" id="repD" name="repD">
+            <div class="c100" id="submit">
+                <input type="submit" value="Envoyer">
+            </div>
+        </form>
+</body>
+</html>
+
+<?php
+//* bouclette
+if (isset($_POST['quest']) && !empty($_POST['quest']) && 
+isset($_POST['repA'])  && !empty($_POST['repA']) &&
+isset($_POST['repB'])  && !empty($_POST['repB']) &&
+isset($_POST['repC'])  && !empty($_POST['repC']) &&
+isset($_POST['repD'])  && !empty($_POST['repD']))
+{
+
+$servname = 'localhost';
+$dbname = 'quizz';
+$user = 'root';
+$pass = '';
+try{
+    $dbco = new PDO("mysql:host=$servname;dbname=$dbname", $user, $pass);
+    $dbco->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+
+$sth = $dbco->prepare("
+INSERT INTO questions (Question, Reponse_A, Reponse_B, Reponse_C, Reponse_D)
+VALUES (:Question, :Reponse_A, :Reponse_B, :Reponse_C, :Reponse_D)
+");
+$sth->bindParam(':Question', $quest);
+$sth->bindParam(':Reponse_A', $repA);
+$sth->bindParam(':Reponse_B', $repB);
+$sth->bindParam(':Reponse_C', $repC);
+$sth->bindParam(':Reponse_D', $repD);
+
+$quest = $_POST['quest']; $repA = $_POST['repA']; $repB = $_POST['repB']; $repC = $_POST['repC']; $repD = $_POST['repD']; 
+$sth->execute ();
+echo 'went good';
+}
+catch(PDOException $e){
+echo "Erreur : " . $e->getMessage();
+}
+}
+
+?>
---
+++ index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <title>OnlineFormaQuizz</title>
+</head>
+<h1>Tableau de Bord</h1>
+
+<div class="container"></div>
+<a href="add_question.php" > <button type="button" class="btn btn-success">Ajouter une question</button></a><br>
+<a href="quizz.php" > <button type="button" class="btn btn-success">Tester le Quizz</button></a>
+</div>
+
+<br class="nettoyeur"/>
+<div class="container">
+  <div class="row align-items-start">
+    <div class="col">
+<button type="button" class="btn btn-success">Question 1</button>
+<button type="button" class="btn btn-warning">Modifier</button>
+<button type="button" class="btn btn-danger">Suprimer</button>
+</div><br>
+
+    <div class="col">
+<button type="button" class="btn btn-success">Question 2</button>
+<button type="button" class="btn btn-warning">Modifier</button>
+<button type="button" class="btn btn-danger">Suprimer</button>
+</div><br>
+
+    <div class="col">
+<button type="button" class="btn btn-success">Question 3</button>
+<button type="button" class="btn btn-warning">Modifier</button>
+<button type="button" class="btn btn-danger">Suprimer</button>
+</div><br class="nettoyeur">
+
+<div class="container">
+  <div class="row align-items-start">
+<div class="col">
+<button type="button" class="btn btn-success">Question 4</button>
+<button type="button" class="btn btn-warning">Modifier</button>
+<button type="button" class="btn btn-danger">Suprimer</button>
+</div><br>
+
+<div class="col">
+<button type="button" class="btn btn-success">Question 5</button>
+<button type="button" class="btn btn-warning">Modifier</button>
+<button type="button" class="btn btn-danger">Suprimer</button>
+</div><br>
+
+<div class="col">
+<button type="button" class="btn btn-success">Question 6</button>
+<button type="button" class="btn btn-warning">Modifier</button>
+<button type="button" class="btn btn-danger">Suprimer</button>
+</div><br>
+
+<?php
+
+$servname = 'localhost';
+$dbname = 'quizz';
+$user = 'root';
+$pass = '';
+
+?>
+
+
+<body>
+    
+</body>
+</html>
